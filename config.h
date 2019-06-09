8122a4ada69f390d46760f9933da4373c5bc3e3f
Khue Doan <khuedoan98@gmail.com>
2019-06-09 18:55:13 +0700

    Add Dracula color scheme

--- config.h
+++ config.h
@@ -3,9 +3,9 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#F8F8F2",   /* after initialization */
+	[INPUT] =  "#BD93F9",   /* during input */
+	[FAILED] = "#FF5555",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
