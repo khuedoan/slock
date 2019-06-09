1218a3b63d28e65cc8dce234026978ea57d3a512
Khue Doan <khuedoan98@gmail.com>
2019-06-09 19:57:00 +0700

    Change default message

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char *colorname[NUMCOLS] = {
 static const int failonclear = 1;
 
 /* default message */
-static const char * message = "Suckless: Software that sucks less.";
+static const char * message = "Enter password to unlock";
 
 /* text color */
 static const char * text_color = "#ffffff";
